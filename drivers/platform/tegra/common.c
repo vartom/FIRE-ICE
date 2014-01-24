f18bf30b401623a639e1f184ec8ffb19dab12050
Bo Yan <byan@nvidia.com>
2014-01-23 20:49:51 -0800

    arm: tegra: always support pre-silicon platform
    this reduces cost of supporting multiple platforms in the life
    cycle of chip software development.
    
    Change-Id: I3642b81cad2f5edfff5b0caf72360e0a29da1863
    Signed-off-by: Bo Yan <byan@nvidia.com>
    Reviewed-on: http://git-master/r/359662
    Reviewed-by: Automatic_Commit_Validation_User
    
    Conflicts:
    	arch/arm/mach-tegra/common.c

--- drivers/platform/tegra/common.c
+++ drivers/platform/tegra/common.c
@@ -2391,7 +2391,6 @@ void __init tegra_init_late(void)
 	tegra_powergate_debugfs_init();
 }
 
-#ifdef CONFIG_TEGRA_PRE_SILICON_SUPPORT
 #define ASIM_SHUTDOWN_REG	0x538f0ffc
 
 static void asim_power_off(void)
@@ -2440,7 +2439,6 @@ static int __init asim_enet_smc91x_init(void)
 }
 
 rootfs_initcall(asim_enet_smc91x_init);
-#endif
 #endif
 
 #if defined(CONFIG_SMSC911X)
