7ea47d24ef90c6e18fdae2c13cc0f8776f1e73f3
Laura Abbott <lauraa@codeaurora.org>
2014-02-11 12:37:49 -0800

    arm64: Export caching APIs
    External modules may need to use caching APIs. Export the symbols
    to allow their use.
    
    Change-Id: I72a862608d37dedf0980ee2790ffb93a9de82221
    Signed-off-by: Laura Abbott <lauraa@codeaurora.org>

--- arch/arm64/kernel/arm64ksyms.c
+++ arch/arm64/kernel/arm64ksyms.c
@@ -27,6 +27,7 @@
 #include <linux/uaccess.h>
 #include <linux/io.h>
 
+#include <asm/cacheflush.h>
 #include <asm/checksum.h>
 
 EXPORT_SYMBOL(copy_page);
@@ -56,6 +57,11 @@ EXPORT_SYMBOL(clear_bit);
 EXPORT_SYMBOL(test_and_clear_bit);
 EXPORT_SYMBOL(change_bit);
 EXPORT_SYMBOL(test_and_change_bit);
+
+	/* caching functions */
+EXPORT_SYMBOL(__dma_inv_range);
+EXPORT_SYMBOL(__dma_clean_range);
+EXPORT_SYMBOL(__dma_flush_range);
 
 #ifdef CONFIG_FUNCTION_TRACER
 EXPORT_SYMBOL(_mcount);
