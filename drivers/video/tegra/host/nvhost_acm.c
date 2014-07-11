5c014a1ce4307a5fdc3ddbcab33663e0699fa1f2
Arto Merilainen <amerilainen@nvidia.com>
2014-07-11 18:21:14 +0300

    video: tegra: host: Fix build breakage
    This patch fixes a build breakage in case where generic power
    domains have been disabled.
    
    Bug 1525992
    
    Change-Id: I72d5f76fd392be9fe9611c3f2c1ade074dab5bbe
    Signed-off-by: Arto Merilainen <amerilainen@nvidia.com>
    Reviewed-on: http://git-master/r/440442

--- drivers/video/tegra/host/nvhost_acm.c
+++ drivers/video/tegra/host/nvhost_acm.c
@@ -777,6 +777,8 @@ int _nvhost_module_add_domain(struct generic_pm_domain *domain,
 	struct platform_device *pdev, bool client)
 {
 	int ret = 0;
+
+#ifdef CONFIG_PM_GENERIC_DOMAINS
 	struct nvhost_device_data *pdata;
 	struct dev_power_governor *pm_domain_gov = NULL;
 
@@ -784,7 +786,6 @@ int _nvhost_module_add_domain(struct generic_pm_domain *domain,
 	if (!pdata)
 		return -EINVAL;
 
-#ifdef CONFIG_PM_GENERIC_DOMAINS
 	if (!pdata->can_powergate)
 		pm_domain_gov = &pm_domain_always_on_gov;
 
