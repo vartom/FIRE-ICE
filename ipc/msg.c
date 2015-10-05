b8a503cd4066b2dfed99ae47dc4cf18094562dad
USBhost <josiahspore@gmail.com>
2015-10-04 17:53:21 -0700

    Merge remote-tracking branch 'common/android-3.10' into 9.y
    revert this when it goes to android-tegra-3.10
    
    Initialize msg/shm IPC objects before doing ipc_addid()
    ipc/sem.c: fully initialize sem_array before making it visible
    ipc: fix race with LSMs
    
    Signed-off-by: USBhost <josiahspore@gmail.com>

--- ipc/msg.c
+++ ipc/msg.c
@@ -202,13 +202,6 @@ static int newque(struct ipc_namespace *ns, struct ipc_params *params)
 		return retval;
 	}
 
-	/* ipc_addid() locks msq upon success. */
-	id = ipc_addid(&msg_ids(ns), &msq->q_perm, ns->msg_ctlmni);
-	if (id < 0) {
-		ipc_rcu_putref(msq, msg_rcu_free);
-		return id;
-	}
-
 	msq->q_stime = msq->q_rtime = 0;
 	msq->q_ctime = get_seconds();
 	msq->q_cbytes = msq->q_qnum = 0;
@@ -220,6 +213,13 @@ static int newque(struct ipc_namespace *ns, struct ipc_params *params)
 
 	ipc_unlock_object(&msq->q_perm);
 	rcu_read_unlock();
+
+	/* ipc_addid() locks msq upon success. */
+	id = ipc_addid(&msg_ids(ns), &msq->q_perm, ns->msg_ctlmni);
+	if (id < 0) {
+		ipc_rcu_putref(msq, msg_rcu_free);
+		return id;
+	}
 
 	return msq->q_perm.id;
 }
