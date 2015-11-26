9360182ea32c4f93a80517549a4bc908df4f7694
USBhost <josiahspore@gmail.com>
2015-11-26 09:38:53 -0800

    does this fix the resume bug that sometimes the screen will not turn on

--- arch/arm/mach-tegra/tegra12_clocks.c
+++ arch/arm/mach-tegra/tegra12_clocks.c
@@ -9270,7 +9270,7 @@ struct tegra_cpufreq_table_data *tegra_cpufreq_table_get(void)
 #define GRANULARITY_KHZ   25500
 #define GRANULARITY_END 1020000
 #define CPU_THROTTLE_FREQ 408000
-#define CPU_SUSPEND_FREQ  408000
+#define CPU_SUSPEND_FREQ  510000
 
 struct tegra_cpufreq_table_data *tegra_cpufreq_table_get(void)
 {
