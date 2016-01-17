6e7f96dda52885117357cfc7df2e45a4a0566f4c
USBhost <josiahspore@gmail.com>
2016-01-17 15:13:14 -0800

    Revert "make CMA NORMAL_NC instead of DEVICE_nGnRE"
    This reverts commit 221c4959058645b54a1459178b7fff994377c68a.

--- arch/arm64/include/asm/mach/map.h
+++ arch/arm64/include/asm/mach/map.h
@@ -21,7 +21,7 @@ struct map_desc {
 	unsigned int type;
 };
 
-#define MT_MEMORY_KERNEL_EXEC 5
+#define MT_MEMORY_DMA_READY 15
 
 #ifdef CONFIG_MMU
 extern void iotable_init(struct map_desc *, int);
